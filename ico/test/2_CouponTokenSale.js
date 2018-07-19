6fcda1d69d1967c62739d8fb4f90aacacb1863e5
palani <palani@mezzofy.com>
2018-07-19 15:46:33 +0530

    modified:   ico/test/2_CouponTokenSale.js

--- ico/test/2_CouponTokenSale.js
+++ ico/test/2_CouponTokenSale.js
@@ -1,5 +1,6 @@
 const CCTCoinSale = artifacts.require("CouponTokenSale.sol");
 const CCTCoin = artifacts.require("CouponToken.sol");
+const CCTBounty =artifacts.require("CouponTokenBounty.sol");
 
  contract("Coupon Token Sale addFounders Test Cases", (accounts) => {
     const owner = accounts[0];
@@ -146,7 +147,7 @@ contract("Coupon Coin Token airDrop Test",(accounts)=>{
     });
   
     it("airDrop program should not elegible for Owner/Fund/Treasury/Contigency address",async()=>{
-      var airDroppers = [accounts[5],accounts[6],accounts[7]];
+      var airDroppers = [accounts[2],accounts[2],accounts[7]];
       var tokens= 1001;   
 
       
@@ -365,3 +366,178 @@ contract("Coupon Coin Token airDrop Test",(accounts)=>{
       }   
     });  
   });
+  contract("Coupon Coin Token addReferrer Test",(accounts)=>{
+ 
+    const owner = accounts[0];
+    let token = null;
+    let sale = null;
+  
+    beforeEach("setup contract for each test", async () => {
+      token = await CCTCoin.new({from: owner });
+      sale = await CCTCoinSale.new( token.address, { from: owner });
+      await token.setContractAddresses(sale.address,0,0);
+      await sale.setupSale(accounts[2],accounts[3],accounts[4]); 
+      await sale.setEth2Cents(45000);
+      await sale.startSale();
+    });
+  
+    it('buyFiat using addReferrer',async()=>{
+      var buyer = accounts[5];
+      var referrer = accounts[6];
+      var cents= 6*100; 
+      try {
+        await sale.addReferrer(buyer,referrer);
+        await sale.buyFiat(buyer,cents);
+        var tokenbalance=await token.balanceOf(buyer);
+        //console.log('Buyer:',tokenbalance.toNumber()/(10**18));
+        var tokenbalance1=await token.balanceOf(referrer);
+        //console.log('Referrer:',tokenbalance1.toNumber()/(10**18));
+        }catch(err) {
+          assert(false, 'buyFiat Failed.');
+      }   
+      var tokenbalance=await token.balanceOf(accounts[5]);
+      if(tokenbalance == 0)
+        assert(false,'buyFiat Failed. But tokens not available into users.')
+    }); 
+    
+  
+    it('owner/admin/founder should not be a referrer',async()=>{
+      var cents= 6*100; 
+       try {
+        await sale.addReferrer(owner,accounts[2]);
+        throw(1);
+        } catch(err) {
+          if(err == 1)
+            assert(false, 'owner not allowed to be a referrer. validation not handled.');
+      }   
+    });  
+    
+    it('only owner should call this function.',async()=>{
+      var cents= 6*100; 
+       try {
+        await sale.addReferrer(accounts[5],accounts[6], {from:accounts[6]});
+        throw(1);
+        }catch(err) {
+          if(err == 1)
+            assert(false, 'owner only call this function. validation not handled.');
+      }   
+    }); 
+  
+  });  
+  contract("Coupon Coin Token buy Test",(accounts)=>{
+
+    const owner = accounts[0];
+    const admin = accounts[1];
+    const fund = accounts[2];
+    const user = accounts[3];
+  
+    let token = null;
+    let sale = null;
+    let sale1=null;
+  
+    beforeEach("setup contract for each test", async()=>{
+      token = await CCTCoin.new({from: owner });
+      sale = await CCTCoinSale.new( token.address, { from: owner });
+      await token.setContractAddresses(sale.address,0,0);
+      await sale.setupSale(accounts[2],accounts[3],accounts[4]); 
+      await sale.setEth2Cents(45000);
+      await sale.startSale();
+    });  
+  
+    it("buy should transfer tokens to the user",async()=>{
+      try{
+        web3.eth.getBalance(accounts[5], function(err,res){
+          console.log(res/(10**18)); 
+        });  
+        await sale.sendTransaction({ value: web3.toWei(1, 'ether'), from: accounts[5] });
+        web3.eth.getBalance(accounts[5], function(err,res){
+          console.log(res/(10**18)); 
+        });  
+        var tknBalanceOf4= await token.balanceOf(accounts[5]);
+        console.log('User 1 Balance:',tknBalanceOf4.toNumber()/(10**18));
+      }catch(err)
+      {
+        assert(false,'Sendtransaction/buy failed.buy not working properly.');
+      }
+    });
+  });
+  contract("Coupon Coin Token endSale Test",(accounts)=>{
+    const owner = accounts[0];
+  
+    var buyer1 = accounts[6];
+    var buyer2 = accounts[7];
+    var buyer3 = accounts[8];
+    var buyer4 = accounts[9];
+  
+    var BountyTotalTokens = 15000000;
+    var cents1= 6 * 30000000; // 30 million
+    var cents2= 7 * 60000000; // 60 million
+    var cents3= 8 * 90000000; // 90 million
+    var cents4= 9 *120000000; // 120 million    
+  
+    let token = null;
+    let sale = null;
+    let bounty = null;
+  
+    beforeEach("setup contract for each test", async () => {
+      token = await CCTCoin.new({from: owner });
+      sale = await CCTCoinSale.new( token.address, { from: owner });
+      
+      bounty = await CCTBounty.new(token.address,sale.address);
+      sale.setBountyAddr(bounty.address);
+
+      await token.setContractAddresses(sale.address,bounty.address,0);
+      await sale.setupSale(accounts[2],accounts[3],accounts[4]); 
+      await sale.setEth2Cents(45000);
+      await sale.startSale();
+  
+      await bounty.createBounty(100*(10**18)); 
+    });
+  
+    it("endsale validation",function (done){
+      try {
+        bounty.createBounty(1000*(10**18));
+        var bountyCreate = bounty.BountyAction();      
+        bountyCreate.watch(async function(err, result){
+          bountyCreate.stopWatching();             
+          if(err){
+            console.log(err);
+            return done(err);
+          }
+          await sale.buyFiat(buyer1,cents1);
+          await sale.buyFiat(buyer2,cents2);
+    
+          var newBountyId = result.args.newBountyId;
+          bounty.activateBounty(newBountyId);
+          var bountyremaining = await sale.remainingBountyTokens();
+          if(BountyTotalTokens == bountyremaining)
+            assert(false,'Bounty Program error.Bounty token balance handled properly.');
+          bounty.fullfillmentBounty(newBountyId,buyer1);
+          bounty.fullfillmentBounty(newBountyId,buyer2);
+          tknBalanceOf4= await token.balanceOf(buyer1);
+          console.log('User 1 Balance:',tknBalanceOf4.toNumber()/(10**18));
+          tknBalanceOf5=await token.balanceOf(buyer2);
+          console.log('User 2 Balance:',tknBalanceOf5.toNumber()/(10**18));
+  
+          await sale.buyFiat(buyer3,cents3);
+          await sale.buyFiat(buyer4,cents4);
+  
+          tknBalanceOf4= await token.balanceOf(buyer3);
+          console.log('User 3 Balance:',tknBalanceOf4.toNumber()/(10**18));
+          tknBalanceOf5=await token.balanceOf(buyer4);
+          console.log('User 4 Balance:',tknBalanceOf5.toNumber()/(10**18));
+  
+          var treassuryBalance = await token.balanceOf(accounts[3]);
+          console.log('Treassury Remaining Before endSale:',treassuryBalance.toNumber()/(10**18));
+          await sale.endSale();
+          treassuryBalance = await token.balanceOf(accounts[3]);
+          console.log('Treassury Remaining After endSale :',treassuryBalance.toNumber()/(10**18));
+          done();
+        })        
+       }catch(err) {
+          assert(false, 'createBounty Failed');
+      }   
+    });  
+  
+  
+  });  
